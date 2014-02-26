4b49cf31515ad8b09c1b40fc582b52a0157d6243
P.-Y. Bourguignon <bourguig@mis.mpg.de>
2014-02-26 18:11:19 +0100

    Modified parallelization scheme

--- lib/ABCmp.py
+++ lib/ABCmp.py
@@ -6,27 +6,25 @@ Created on Wed Feb 12 15:43:00 2014
 """
 
 import multiprocessing
+from ctypes import c_int, c_double
+from timeit import Timer
 import sys
-from ctypes import c_double
-import importlib
 import os
+import importlib
 
-DEBUG = False
+def debug(msg):
+    sys.stderr.write(msg)
+    sys.stderr.flush()
 
 class ABCmp(object):
-    def __init__(self, data, f_prior, f_model, f_summarize, nsamplers=2, nevaluators=2):
-        if DEBUG is True:
-            sys.stderr.write("Debug mode activated\n")
-        self.nsamplers = nsamplers
-        self.nevaluators = nevaluators
-        self.f_summarize, self.f_prior, self.f_model = \
-                                            f_summarize, f_prior, f_model        
-        self.data_summary = self.f_summarize(data)
+    def __init__(self, data_summary, f_prior, f_statistics, nworkers=2):
+        self.nworkers = nworkers
+        self.f_prior, self.f_statistics = f_prior, f_statistics
+        self.data_summary = data_summary
         self.f_distance = self.learn_distance()
-        
-    def learn_distance(self, size=1000):
 
-        samples = [self.f_summarize(self.f_model(self.f_prior())) \
+    def learn_distance(self, size=100):
+        samples = [self.f_statistics(self.f_prior()) \
                     for ii in range(size)]
         w = {}
         for k in self.data_summary.keys():
@@ -37,113 +35,77 @@ class ABCmp(object):
         return (lambda s: sum([(s[k]-v)**2/w[k] \
                             for k,v in self.data_summary.iteritems()]))
         
-    def sample(self, nsamples, acc_ratio):
-
-        queue = multiprocessing.Queue(1000)
-        res = multiprocessing.Queue(1000)
-        
-        samplers = [Sampler(queue, self.f_prior, 
-                                   self.f_model, 
-                                   self.f_summarize) \
-                                for ii in range(self.nsamplers)]
-        
-        
-        
-        for w in samplers:
-            w.start()
-
+    def sample(self, nsamples, acc_ratio, queue_size=1000):
         ntest = max(1000, int(2.0/acc_ratio))
-        samples = []
-
-        for ii in range(ntest):
-            params, summary = queue.get()
-            dist = self.f_distance(summary)
-            samples.append(dist)
+        samples = [self.f_distance(self.f_statistics(self.f_prior()))\
+                    for ii in range(ntest)]
         
         s_samples = sorted(samples)
-        
-        thr = [s_samples[int(k*ntest/50)] for k in range(10)]
-        t_high = multiprocessing.Value(c_double,thr[-1])
-    
-        del samples, s_samples    
-                
-    
-        
-        evaluators = [Evaluator(queue, res, t_high, self.f_distance) \
-                        for ii in range(self.nevaluators)]
-        for s in evaluators:
-            s.start()
 
-        samples = [[]]*len(thr)
+        queue = multiprocessing.Queue(queue_size)
+        t_high = multiprocessing.RawValue(c_double, s_samples[int(2*ntest*acc_ratio)])
+        ndiscards = [multiprocessing.Value(c_int, 0)\
+                        for ii in range(self.nworkers)]
+        workers = [Sampler(queue, ndiscards[ii], t_high,
+                           self.f_prior, 
+                           self.f_statistics, 
+                           self.f_distance)
+                                for ii in range(self.nworkers)]
         
-        sys.stderr.write("Starting sampling...\n")
-        sys.stderr.flush()        
+        for w in workers:
+            w.start()
+
+        samples = []
         
-        for ii in range(nsamples):
-            if ii % int(nsamples/100) == 1:
-                sys.stderr.write("\rSampling " + str(int(ii / (nsamples/100))) + "% complete, queue sizes " + str(queue.qsize()) + ' ' + str(res.qsize()) + "                  ")
-                sys.stderr.flush()
-            params, dist = res.get()
-            if params is None:
-                continue
-            for ss in range(len(thr)):
-                if dist < thr[ss]:
-                    samples[ss].append((params, dist))
+        debug("Starting sampling...\n")
+        nvalids = 0
+        clock = Timer()
+        log_time = clock.timer()
+        while True:
+            if clock.timer() - log_time > 5:
+                nd = sum([ndiscards[ii].value for ii in range(self.nworkers)])
+                ndraws = nd + nvalids
+                debug("\rSampling " + 
+                     str(int(ndraws / (nsamples/100))) + 
+                     "% complete")
+                if ndraws > nsamples:
                     break
-        
-        for s in samplers:
-            s.terminate()
-        for w in evaluators:
+            
+            params, dist = queue.get()
+            nvalids += 1
+            samples.append((params, dist))
+            
+        for w in workers:
             w.terminate()
-        sys.stderr.write("\rSampling completed                                   \n")
-
-#        if DEBUG is True:
-#            sys.stderr.write("Lower queue size: " + str(len(samples[0])) + '\n')
-#            sys.stderr.write("Higher queue size: " + str(len(samples[1])) + '\n')
-
-
-
-        results = []
-        cur_slice = 0
-        ntarget = int(nsamples * acc_ratio)
-        while len(results) < ntarget:
-            if len(samples[cur_slice]) + len(results) < ntarget:
-                results = results + [x[0] for x in samples[cur_slice]]
-            else:
-                s_slice = sorted(samples[cur_slice], key=lambda x: x[1])
-                results = results + [x[0] for x in s_slice[0:ntarget-len(results)]]
-        
+        debug("\rSampling completed      \n")
+        debug("Queue size: " + str(nvalids) + '\n')
+        #TODO
+        # Make sure the queue is fully consumed, otherwise
+        # sample statistics are invalid
+        results = [x[0] for x in sorted(samples)[0:int(nsamples*acc_ratio)]]
         return results
 
     
 
 class Sampler(multiprocessing.Process):
-    def __init__(self, queue, f_prior, f_model, f_summarize):
+    def __init__(self, queue, ndiscards, t_high,
+                 f_prior, f_statistics, f_distance):
         multiprocessing.Process.__init__(self)
-        self.queue = queue
-        self.f_prior, self.f_model, self.f_summarize = f_prior, f_model, f_summarize
+        self.queue, self.ndiscards, self.t_high = \
+                            queue, ndiscards, t_high
+        self.f_prior, self.f_statistics, self.f_distance = \
+                            f_prior, f_statistics, f_distance
+        
+        
     def run(self):
         while True:
             params = self.f_prior()
-            obs = self.f_model(params)
-            self.queue.put((params, self.f_summarize(obs)))
-
-class Evaluator(multiprocessing.Process):
-    def __init__(self, queue, res, t_high, f_distance):
-        multiprocessing.Process.__init__(self)
-        self.queue = queue
-        self.res = res
-        self.t_high = t_high
-        self.f_distance = f_distance
-    def run(self):
-        while True:
-            params, summary = self.queue.get()
-            dist = self.f_distance(summary)
+            dist = self.f_distance(self.f_statistics(params))
             if dist < self.t_high.value:
-                self.res.put((params, dist))
+                self.queue.put((params, dist))
             else:
-                self.res.put((None, None))
-
+                self.ndiscards.value += 1
+            
 if __name__ == '__main__':
     nsamples = int(sys.argv[1])
     acc_ratio = float(sys.argv[2])
@@ -163,7 +125,7 @@ if __name__ == '__main__':
                 'beta_y':  (2.0,10),
                 'beta_o':  (2.0,10),}
 
-    sampler = ABCmp(data, lambda: mod.lt_prior(h_params), lambda x: mod.lt_noisy_obs(mod.lt_model(x,4)), mod.lt_summarize, nworkers=ncpus)
+    sampler = ABCmp(mod.lt_summarize(data), lambda: mod.lt_prior(h_params), lambda x: mod.lt_summarize(mod.lt_noisy_obs(mod.lt_model(x,4))), nworkers=ncpus)
     res = sampler.sample(nsamples,acc_ratio)
     for val in res:
         print mod.lt_format(val)
