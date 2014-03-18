7255ada39bdbaf1df5587c3e26eb4cc78454a369
P.-Y. Bourguignon <pierre-yves.bourguignon@mis.mpg.de>
2014-03-18 09:56:47 +0100

    Completed setup

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from distutils.core import setup
 
 setup(name='python-abc',
       version='0.1',
+      description="A parallel implementation of the ABC sampling method",
+      author="P.-Y. Bourguignon",
+      author_email="pybourguignon@gmail.com",
+      url="http://www.github.com/pbourguignon/python-abc",
+      package_dir={ '': 'lib'},
+      scripts=['bin/abc',],
       py_modules=['ABC','ABCmp'])
 
